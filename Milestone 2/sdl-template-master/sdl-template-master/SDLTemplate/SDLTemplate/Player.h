db3071ca0b1c9522526972b5bb1ed2f16400507d
Johann <johannmiguel.cagara@benilde.edu.ph>
2023-09-22 15:03:44 +0800

    Milestone 2

---
+++ Milestone 2/sdl-template-master/sdl-template-master/SDLTemplate/SDLTemplate/Player.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "GameObject.h"
+#include "common.h"
+#include "draw.h"
+#include "SoundManager.h"
+#include "Bullet.h"
+#include <vector>
+class Player :
+    public GameObject
+{
+public:
+	~Player();
+	void start();
+	void update();
+	void draw();
+private:
+	SDL_Texture* texture;
+	Mix_Chunk* sound;
+	int x;
+	int y;
+	int width;
+	int height;
+	int speed;
+	float reloadTimeFront;
+	float reloadTimeSides;
+	float currentReloadTime;
+	std::vector<Bullet*> bullets;
+};
+
