57762053c7b7f1bb3d3ed97804f4934c779e5262
Johann <johannmiguel.cagara@benilde.edu.ph>
2023-09-22 19:49:14 +0800

    Milestone 3

---
+++ Milestone 3/sdl-template-master/sdl-template-master/SDLTemplate/SDLTemplate/Enemy.cpp
@@ -0,0 +1,99 @@
+#include "Enemy.h"
+#include "Scene.h"
+
+Enemy::Enemy()
+{
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::start()
+{
+	// load texture
+	texture = loadTexture("gfx/enemy.png");
+
+	directionX = -1;
+	directionY = 1;
+	width = 0;
+	height = 0;
+	speed = 2;
+	reloadTime = 60;
+	currentReloadTime = 0;
+	directionChangeTime = (rand() % 300) + 180;
+	currentDirectionChangeTime = 0;
+
+	SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+
+	sound = SoundManager::loadSound("sound/334227__jradcoolness__laser.ogg");
+}
+
+void Enemy::update()
+{
+	x += directionX * speed;
+	y += directionY * speed;
+
+	if (currentDirectionChangeTime > 0) {
+		currentDirectionChangeTime--;
+	}
+	if (currentDirectionChangeTime == 0) {
+		directionY = -directionY;
+		currentDirectionChangeTime = directionChangeTime;
+	}
+
+	if (currentReloadTime > 0) {
+		currentReloadTime--;
+	}
+
+	if (currentReloadTime == 0) {
+		float dx = -1;
+		float dy = 0;
+
+		calcSlope(playerTarget->getPositionX(), playerTarget->getPositionY(), x, y, &dx, &dy);
+
+		SoundManager::playSound(sound);
+		Bullet* bullet = new Bullet(x + width, y - 2 + height / 2, dx, dy, 10);
+		bullets.push_back(bullet);
+		getScene()->addGameObject(bullet);
+		bullet->start();
+
+		currentReloadTime = reloadTime;
+	}
+
+	for (int i = 0; i < bullets.size(); i++) {
+		if (bullets[i]->getPositionX() < 0) {
+			Bullet* bulletToErase = bullets[i];
+			bullets.erase(bullets.begin() + i);
+			delete bulletToErase;
+
+			break;
+		}
+	}
+}
+
+void Enemy::draw()
+{
+	blit(texture, x, y);
+}
+
+void Enemy::setPlayerTarget(Player* player)
+{
+	playerTarget = player;
+}
+
+void Enemy::setPosition(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+int Enemy::getPositionY()
+{
+	return y;
+}
+
+int Enemy::getPositionX()
+{
+	return x;
+}
---
+++ Milestone 3/sdl-template-master/sdl-template-master/SDLTemplate/SDLTemplate/Enemy.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "GameObject.h"
+#include "common.h"
+#include "draw.h"
+#include "SoundManager.h"
+#include "Bullet.h"
+#include <vector>
+#include "util.h"
+#include "Player.h"
+
+class Enemy : public GameObject
+{
+public:
+	Enemy();
+	~Enemy();
+	void start();
+	void update();
+	void draw();
+	void setPlayerTarget(Player* player);
+	void setPosition(int x, int y);
+	int getPositionX();
+	int getPositionY();
+private:
+	SDL_Texture* texture;
+	Mix_Chunk* sound;
+	Player* playerTarget;
+	int x;
+	int y;
+	float directionX;
+	float directionY;
+	int width;
+	int height;
+	int speed;
+	float reloadTime;
+	float currentReloadTime;
+	float directionChangeTime;
+	float currentDirectionChangeTime;
+	std::vector<Bullet*> bullets;
+};
+
